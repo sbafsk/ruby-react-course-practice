a3b71204a073c460510b1754acb90f9258fd8611
sbafsk <razor1836@gmail.com>
2021-10-14 16:03:34 -0300

    tictactoe progress

--- sections/11_project_tic_tac_toe_ruby/board.rb
+++ sections/11_project_tic_tac_toe_ruby/board.rb
@@ -1,30 +1,9 @@
 # frozen_string_literal: true
 
-# 67.
-# - A board (instance of the Board class)  "has-a" 2-dimensional array
-# saved in the @grid variable.
-# - The Board class could easily be modified to create a 2d grid of any size.
-# - The "cells" on the board are symbols, but i could also have used strings,
-# or defined a Cell class which would have a string or symbol value.
-#
-# 68.
-# - My solution involved nested iterators blocks.
-# - We go throught each row, then each cell inside the row.
-# - we use #any? (row) and #all? (cells)
-#
-# 69.
-# - The only big difference between checking rows vs. columns is that
-# we can't iterate over the colums directly, so I used a range
-# from (0...WIDTH) to iterate over the indexes.
-# - The range uses three dots to exclude WIDTH(3) because the lenght
-# of an array is always one more than the last index (indexes start at 0) lol
-
 # class Board
 class Board
   WIDTH = 3
   HEIGHT = WIDTH
-
-  attr_accessor :grid
 
   def initialize
     @grid = Array.new(HEIGHT) { Array.new(WIDTH, :" ") }
@@ -38,6 +17,12 @@ class Board
       end
     end
     puts
+  end
+
+  def winner?(marker)
+    row_win?(marker) ||
+      column_win?(marker) ||
+      diagonal_win?(marker)
   end
 
   def row_win?(marker)
@@ -57,15 +42,29 @@ class Board
   end
 
   def diagonal_win?(marker)
-    return true if (0...WIDTH).all? do |i|
-      @grid[i][i] == marker
+    # [-1, 1].any? do |sign|
+    [
+      (->(i) { i }),
+      (->(i) { -(i + 1) })
+    ].any? do |proc|
+      (0...WIDTH).all? do |i|
+        # second_index = sign.positive? ? i : sign * (i + 1)
+        # @grid[i][second_index] == marker
+        @grid[i][proc.call(i)] == marker
+      end
     end
+  end
 
-    return true if (0...WIDTH).all? do |i|
-      @grid[i][-(i + 1)] == marker
+  def [](y, x)
+    @grid[y][x]
+  end
+
+  def []=(y, x, something)
+    if @grid[y][x] == :" " && [:x, :o].include?(something)
+      @grid[y][x] = something
+    else
+      false
     end
-
-    false
   end
 end
 
@@ -81,17 +80,22 @@ end
 
 @b = Board.new
 
-@b.grid[2][0] = :o
-@b.grid[2][1] = :o
-@b.grid[2][2] = :o
-print_and_check
+# @b.grid[2][0] = :o
+# @b.grid[2][1] = :o
+# @b.grid[2][2] = :o
+# print_and_check
 
-@b.grid[2][1] = :x
-@b.grid[1][1] = :x
-@b.grid[0][1] = :x
-print_and_check
+# @b.grid[2][1] = :x
+# @b.grid[1][1] = :x
+# @b.grid[0][1] = :x
+# print_and_check
 
-@b.grid[0][0] = :x
-@b.grid[1][1] = :x
-@b.grid[2][2] = :x
-print_and_check
+# @b[0, 0] = :x
+# @b[1, 1] = :x
+# @b[2, 2] = :x
+# print_and_check
+
+# @b[0, 2] = :o
+# @b[1, 1] = :o
+# @b[2, 0] = :o
+# print_and_check
